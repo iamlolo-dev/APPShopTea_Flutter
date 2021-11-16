37db80be2923971c53af9bde79dd937811105e38
masterprog-cmd <xaviabadgome01@gmail.com>
2021-11-16 20:30:34 +0100

    settings

--- lib/timer/main_page_timer.dart
+++ lib/timer/main_page_timer.dart
@@ -1,13 +1,117 @@
+import 'package:email_password_login/view/components/timer/timer/timer.dart';
 import 'package:flutter/material.dart';
 
-class Timer extends StatelessWidget {
+class HomePageTimer extends StatefulWidget {
+  @override
+  State<HomePageTimer> createState() => _HomePageTimerState();
+}
+
+class _HomePageTimerState extends State<HomePageTimer> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
-      child: Center(
-        child: Text("Cold Coffee"),
-      ),
+    return Column(
+      children: <Widget>[
+        ListTile(
+          leading: const Icon(
+            Icons.alarm,
+            color: Colors.teal,
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Timer(seg: 60),
+              ),
+            );
+          },
+          title: Text("Green tea"),
+          subtitle: Text("1 minute"),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.navigate_next_outlined,
+                color: Colors.teal,
+              ),
+            ],
+          ),
+        ),
+        ListTile(
+          leading: const Icon(
+            Icons.alarm,
+            color: Colors.teal,
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Timer(seg: 120),
+              ),
+            );
+          },
+          title: Text("Black tea"),
+          subtitle: Text("2 minutes"),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.navigate_next_outlined,
+                color: Colors.teal,
+              )
+            ],
+          ),
+        ),
+        ListTile(
+          leading: const Icon(
+            Icons.alarm,
+            color: Colors.teal,
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Timer(seg: 300),
+              ),
+            );
+          },
+          title: Text("White tea"),
+          subtitle: Text("5 minutes"),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.navigate_next_outlined,
+                color: Colors.teal,
+              ),
+            ],
+          ),
+        ),
+        ListTile(
+          leading: const Icon(
+            Icons.alarm,
+            color: Colors.teal,
+          ),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => Timer(seg: 240),
+              ),
+            );
+          },
+          title: Text("Red tea"),
+          subtitle: Text("4 minutes"),
+          trailing: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.navigate_next_outlined,
+                color: Colors.teal,
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
